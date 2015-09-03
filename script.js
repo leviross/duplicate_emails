6495471a4272c938056972eefe2114c142fe72e7
leviathon <levross@gmail.com>
2015-09-02 20:41:40 -0700

    final, not using splice as that runs a loop behind the scenes

--- script.js
+++ script.js
@@ -6,24 +6,23 @@
 //I cant think of a faster way right now, but there could be one. You have to loop through the main array at least 1 
 //full time and with 1 Billion, that would take longer than 1 second I imagine...
 
+
 function duplicateEmails(emailArray){
 	//initialize a new hash object
     var scanned = {};
-    //in order not to break a loop when you are splicing, you have to start at the end of the array and decrement i
-    //if you increment, as you splice, you are changing the index of everything after that element, and you are skipping
-    //an element every time
-    for(var i = emailArray.length-1; i >= 0 ; i--){
-    	//check if the current email exists as a key in our hash
-    	if(scanned[emailArray[i]]){
-    		//if it does, take it out of our array. 
-            emailArray.splice(i, 1);
-    	}else{
-    		//if it doesnt exist, insert the email into our hash
+    var finalArray = [];
+    //I was originally running a decrementing loop and splicing the non duplicates, that splice runs a loop 
+    //behind the scenes and it ended up being O(n^2), being that I was doing a full loop for every iteration
+    for(var i = 0; i < emailArray.length; i++){
+    	//check if the current email exists as a key in our hash table	
+    	if(!scanned[emailArray[i]]){
+    		//if it doesn't, add it to our final array and to our hash table
     		scanned[emailArray[i]] = 1;
+    		finalArray.push(emailArray[i]);
     	}
     }
     
-    return emailArray;
+    return finalArray;
 }
 
 var emails = ['leviiscool@gmail.com', 'wife@gmail.com', 'leviiscool@gmail.com', 'leviiscool@gmail.com', 'wife@gmail.com',
@@ -31,3 +30,4 @@ var emails = ['leviiscool@gmail.com', 'wife@gmail.com', 'leviiscool@gmail.com', 
 
 
 console.log(duplicateEmails(emails));
+
